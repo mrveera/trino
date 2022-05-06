54cc24f5615bd640328bc9af0c15d4cb72cc8213
Veera Pirla <17177346+mrveera@users.noreply.github.com>
2022-05-06 09:51:21 +0530

    Revert "Support registering catalog dynamically with RESTful API"

--- core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
+++ core/trino-main/src/main/java/io/trino/server/ServerMainModule.java
@@ -64,11 +64,9 @@ import io.trino.memory.NodeMemoryConfig;
 import io.trino.metadata.AnalyzePropertyManager;
 import io.trino.metadata.BlockEncodingManager;
 import io.trino.metadata.CatalogManager;
-import io.trino.metadata.CatalogResource;
 import io.trino.metadata.ColumnPropertyManager;
 import io.trino.metadata.DisabledSystemSecurityMetadata;
 import io.trino.metadata.DiscoveryNodeManager;
-import io.trino.metadata.DynamicCatalogStore;
 import io.trino.metadata.ForNodeManager;
 import io.trino.metadata.FunctionBundle;
 import io.trino.metadata.FunctionManager;
@@ -379,7 +377,6 @@ public class ServerMainModule
 
         // metadata
         binder.bind(StaticCatalogStore.class).in(Scopes.SINGLETON);
-        binder.bind(DynamicCatalogStore.class).in(Scopes.SINGLETON);
         configBinder(binder).bindConfig(StaticCatalogStoreConfig.class);
         binder.bind(MetadataManager.class).in(Scopes.SINGLETON);
         binder.bind(Metadata.class).to(MetadataManager.class).in(Scopes.SINGLETON);
@@ -395,7 +392,6 @@ public class ServerMainModule
         binder.bind(ProcedureRegistry.class).in(Scopes.SINGLETON);
         binder.bind(TableProceduresRegistry.class).in(Scopes.SINGLETON);
         binder.bind(PlannerContext.class).in(Scopes.SINGLETON);
-        jaxrsBinder(binder).bind(CatalogResource.class);
 
         // function
         binder.bind(FunctionManager.class).in(Scopes.SINGLETON);
